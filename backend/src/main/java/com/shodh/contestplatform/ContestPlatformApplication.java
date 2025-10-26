e3f406f3512533b1be2b2a5cb45d7a09c9bac5e0
Tushar <tusharsingh00769@gmail.com>
2025-10-26 08:15:02 +0530

    Added backend source code and Dockerfile for Render deployment

---
+++ backend/src/main/java/com/shodh/contestplatform/ContestPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.shodh.contestplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContestPlatformApplication.class, args);
+    }
+}
